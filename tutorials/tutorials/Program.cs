cd3fde1b237c2b57ecd8d756abb51ac5c9a5dd01
Parkseokje <sjpark@clipplr.com>
2016-04-12 16:01:18 +0900

    sample1, 2 추가

--- tutorials/tutorials/Program.cs
+++ tutorials/tutorials/Program.cs
@@ -10,8 +10,43 @@ namespace tutorials
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Dreamonesys!");
             Console.WriteLine("This is chapter03 branch. you can find it at github.com.");
+
+            ChapterExamples examples = new ChapterExamples();
+
+            // 예제1 : 2의 보수법(2's Complement)에 대해 알아본다.
+            //examples.sample1();
+            // 예제2 : 오버플로우(Overflow) 에 대해 알아본다.
+            examples.sample2();
+        }
+    }
+
+    class ChapterExamples
+    {
+        /// <summary>
+        /// 예제1 : 2의 보수법에 대해 알아본다.
+        /// </summary>
+        public void sample1 ()
+        {
+            byte a = 255;
+            sbyte b = (sbyte)a;
+
+            Console.WriteLine(a); // 출력값 : 255
+            Console.WriteLine(b); // 출력값 : -1 (2의 보수법)
+        }
+
+        /// <summary>
+        /// 예제2 : 오버플로우(Overflow) 에 대해 알아본다.
+        /// </summary>
+        public void sample2()
+        {
+            uint a = uint.MaxValue; // 4,294,967,295 (4bytes, 약 42억개의 수 표현가능)
+
+            Console.WriteLine(a);
+
+            a += 1;
+
+            Console.WriteLine(a); // 출력값 : 0
         }
     }
 }
