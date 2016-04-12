cc1eed46035645e32a481faab4177aab726c6b17
Parkseokje <sjpark@clipplr.com>
2016-04-12 21:11:07 +0900

    - 샘플 목록 보여주는 메소드 추가 - 샘플 실행해주는 메소드 추가 - 샘플 재실행 해주는 코드 추가

--- tutorials/tutorials/Program.cs
+++ tutorials/tutorials/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace tutorials
 {
@@ -10,33 +7,117 @@ namespace tutorials
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is chapter03 branch. you can find it at github.com.");
+            Console.WriteLine("===========================================================================");
+            Console.WriteLine("This is chapter03 branch.");
+            Console.WriteLine("You can find full source code at https://github.com/dreamonesys/DOS_study/.");
+            Console.WriteLine("===========================================================================");
+            Console.WriteLine("");
 
             ChapterExamples examples = new ChapterExamples();
 
-            // 예제1 : 2의 보수법(2's Complement)에 대해 알아본다.
-            //examples.sample1();
-            // 예제2 : 오버플로우(Overflow) 에 대해 알아본다.
-            examples.sample2();
+            string response = "Y";
+
+            // 사용자가 샘플 실행 여부를 묻는 질문에 "Y" 입력시 다시 샘플 목록을 표시한다.
+            do
+            {
+                // 샘플 목록을 표시한다.
+                examples.showExample();
+                // 사용자로부터 번호를 입력받아 샘플을 실행한다.
+                examples.executeSample();
+
+                // 다시 샘플을 실행할지 물어본다.
+                Console.WriteLine("다시 샘플을 실행하시겠습니까? (Y/N)");
+                response = Console.ReadLine();
+
+            } while (response == "Y");
+
         }
     }
 
     class ChapterExamples
     {
         /// <summary>
-        /// 예제1 : 2의 보수법에 대해 알아본다.
+        /// 샘플 목록을 표시한다.
+        /// </summary>
+        public void showExample()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("1. 2의 보수법(2's Complement)에 대해 알아본다.");
+            sb.AppendLine("2. 오버플로우(Overflow) 에 대해 알아본다.");
+            sb.AppendLine("3. 부동 소수점 형식(Float Pointing Types)과 Decimal 형식의 차이점에 대해 알아본다.");
+            sb.AppendLine("4. char 형식의 문자 데이터 출력에 대해 알아본다.");
+            sb.AppendLine("5. string 형식에 대해 알아본다.");
+            sb.AppendLine("6. 논리(Boolean Type)에 대해 알아본다.");
+            sb.AppendLine("7. Object Type 에 대해 알아본다.");
+
+            // 선택 메뉴를 표시한다.
+            Console.WriteLine(sb.ToString());
+
+            // 사용자로부터 번호를 입력받는다.
+            Console.WriteLine("번호를 입력하세요.");
+        }
+
+        /// <summary>
+        /// 샘플을 실행한다.
+        /// </summary>
+        public void executeSample()
+        {
+
+            string line = Console.ReadLine();
+            int num = 0;
+
+            if (int.TryParse(line, out num))
+            {
+                // 0 을 입력할 경우 1번 샘플을 실행한다.
+                num = (num == 0) ? 1 : num;
+
+                switch (num)
+                {
+                    case 1:
+                        this.sample1();
+                        break;
+                    case 2:
+                        this.sample2();
+                        break;
+                    case 3:
+                        this.sample3();
+                        break;
+                    case 4:
+                        this.sample4();
+                        break;
+                    case 5:
+                        this.sample5();
+                        break;
+                    case 6:
+                        this.sample6();
+                        break;
+                    case 7:
+                        this.sample7();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("숫자를 입력하세요!");
+            }
+        }
+
+        /// <summary>
+        /// 예제1. 2의 보수법에 대해 알아본다.
         /// </summary>
         public void sample1 ()
         {
             byte a = 255;
             sbyte b = (sbyte)a;
 
-            Console.WriteLine(a); // 출력값 : 255
-            Console.WriteLine(b); // 출력값 : -1 (2의 보수법)
+            Console.WriteLine(a); // 255
+            Console.WriteLine(b); // -1 (2의 보수법)
         }
 
         /// <summary>
-        /// 예제2 : 오버플로우(Overflow) 에 대해 알아본다.
+        /// 예제2. 오버플로우(Overflow) 에 대해 알아본다.
         /// </summary>
         public void sample2()
         {
@@ -46,7 +127,79 @@ namespace tutorials
 
             a += 1;
 
-            Console.WriteLine(a); // 출력값 : 0
+            Console.WriteLine(a); // 0
+        }
+
+        /// <summary>
+        /// 예제3. 부동 소수점 형식(Float Pointing Types)과 Decimal 형식의 차이점에 대해 알아본다.
+        /// </summary>
+        public void sample3()
+        {
+            float a = 3.14159265358979323846f; // float 형식 변수에 값을 직접 할당하려면, 숫자 뒤에 f 를 붙여준다.
+            Console.WriteLine(a); // 3.141594
+
+            double b = 3.14159265358979323846;
+            Console.WriteLine(b); // 3.14159265358979
+
+            decimal c = 3.141592653589793238462643383279m;
+            Console.WriteLine(c); // 3.1415926535897932384626433833
+        }
+
+        /// <summary>
+        /// 예제4. char 형식의 문자 데이터 출력에 대해 알아본다.
+        /// </summary>
+        public void sample4()
+        {
+            char a = '안'; // " 를 사용할 경우 Cannot implicitly convert type 'string' to 'char' 오류 발생
+            char b = '녕';
+            char c = '하';
+            char d = '세';
+            char e = '요';
+
+            Console.Write(a); // Write : 출력 후 줄바꿈 없음.
+            Console.Write(b);
+            Console.Write(c);
+            Console.Write(d);
+            Console.Write(e);
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// 예제 5. string 형식에 대해 알아본다.
+        /// </summary>
+        public void sample5()
+        {
+            string a = "Hello"; // ' 를 사용할 경우, Too many charaters in character literal 오류 발생.
+            string b = "Dreamonesys!";
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+
+        /// <summary>
+        /// 예제 6. 논리(Boolean Type)에 대해 알아본다.
+        /// </summary>
+        public void sample6()
+        {
+            bool a = true;
+            bool b = false;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+
+        /// <summary>
+        /// 예제 7. Object Type 에 대해 알아본다.
+        /// </summary>
+        public void sample7()
+        {
+            object a = 123; // Integer
+            object b = 3.141592653589793238462643383279m; // Decimal
+            object c = true; // Boolean
+            object d = "안녕하세요."; //string
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
         }
     }
 }
