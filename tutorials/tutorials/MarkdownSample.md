85ea7b237c0043be4e65223eec1e7e6b0789f965
Parkseokje <sjpark@clipplr.com>
2016-04-14 18:06:10 +0900

    연습문제 까지 소스에 최종 반영.

---
+++ tutorials/tutorials/MarkdownSample.md
@@ -0,0 +1,24 @@
+﻿Hello Markdown in VS Code!
+====================
+
+This is a simple introduction to compiling Markdown in VS Code.
+
+Things you'll need:
+
+* [inline link](https://www.npmjs.com/package/marked)
+* [reference link][rl]
+* url link : <http://example.com/>, <me@privacy.net>
+* image : ![alternate text](http://s.wordpress.org/about/images/logos/wordpress-logo-32.png "This is Inline image")
+* small image : ![alternate text][1](http://s.wordpress.org/about/images/wpmini-grey.png "This is Small image.")
+* linked image : [![alternate text](http://s.wordpress.org/about/images/wpmini-grey.png)](http://www.naver.com/ "This is Linked image.")
+
+--- 
+
+각주 [^1] 이렇게 만든다.
+[^1]: 각주다. 
+
+[rl]: http://www.google.com
+
+#### Section **_Title_**
+
+> This block quote is here for your **information**.
--- tutorials/tutorials/Program.cs
+++ tutorials/tutorials/Program.cs
@@ -17,7 +17,7 @@ namespace tutorials
 
             string response = "Y";
 
-            // 사용자가 샘플 실행 여부를 묻는 질문에 "Y" 입력시 다시 샘플 목록을 표시한다.
+            // 사용자가 샘플 실행 여부를 묻는 질문에 "Y(y)" 입력시 다시 샘플 목록을 표시한다.
             do
             {
                 // 샘플 목록을 표시한다.
@@ -27,9 +27,9 @@ namespace tutorials
 
                 // 다시 샘플을 실행할지 물어본다.
                 Console.WriteLine("다시 샘플을 실행하시겠습니까? (Y/N)");
-                response = Console.ReadLine();
+                response = Console.ReadLine().ToLower();
 
-            } while (response == "Y");
+            } while (response == "y");
 
         }
     }
@@ -50,7 +50,13 @@ namespace tutorials
             sb.AppendLine("6. 논리(Boolean Type)에 대해 알아본다.");
             sb.AppendLine("7. Object Type 에 대해 알아본다.");
             sb.AppendLine("8. boxing 과 unboxing 에 대해 알아본다.");
-            sb.AppendLine("9. 형식 변환(Type Conversion)에 대해 알아본다.");
+            sb.AppendLine("9. 형식 사이의 변환(Type Conversion)에 대해 알아본다.");
+            sb.AppendLine("10. 상수(Constants)에 대해 알아본다.");
+            sb.AppendLine("11. 열거형식(Enumerator)에 대해 알아본다.");
+            sb.AppendLine("12. Nullable 형식에 대해 알아본다.");
+            sb.AppendLine("13. 암시적 형식, var 에 대해 알아본다.");
+            sb.AppendLine("14. 공용형식시스템(CTS, Common Type System)에 대해 알아본다.");
+            sb.AppendLine("15. 연습문제");
 
             // 선택 메뉴를 표시한다.
             Console.WriteLine(sb.ToString());
@@ -67,6 +73,8 @@ namespace tutorials
 
             string line = Console.ReadLine();
             int num = 0;
+
+            Console.WriteLine("=============================================");
 
             if (int.TryParse(line, out num))
             {
@@ -101,6 +109,24 @@ namespace tutorials
                         break;
                     case 9:
                         this.sample9();
+                        break;
+                    case 10:
+                        this.sample10();
+                        break;
+                    case 11:
+                        this.sample11();
+                        break;
+                    case 12:
+                        this.sample12();
+                        break;
+                    case 13:
+                        this.sample13();
+                        break;
+                    case 14:
+                        this.sample14();
+                        break;
+                    case 15:
+                        this.sample15();
                         break;
                     default:
                         break;
@@ -233,10 +259,13 @@ namespace tutorials
         }
 
         /// <summary>
-        /// 예제 9. 형식 변환(Type Conversion)에 대해 알아본다.
+        /// 예제 9. 크기가 서로 다른 형식사이의 변환(Type Conversion)에 대해 알아본다.
         /// </summary>
         public void sample9()
         {
+            // 크기가 서로 다른 정수 형식사이의 변환
+            Console.WriteLine("크기가 서로 다른 정수 형식사이의 변환");
+
             sbyte a = 127;
             Console.WriteLine(a);
 
@@ -248,6 +277,193 @@ namespace tutorials
 
             sbyte y = (sbyte)x; // 오버플로우 발생
             Console.WriteLine(y); // -128
+
+            // 크기가 서로 다른 부동 소수점 형식사이의 변환
+            Console.WriteLine("=============================================");
+            Console.WriteLine("크기가 서로 다른 부동 소수점 형식사이의 변환");
+
+            float c = 69.6875f;
+            Console.WriteLine("a(float) : {0}", a);
+
+            double d = (double)c;
+            Console.WriteLine("b(double) : {0}", d);
+            Console.WriteLine("69.6875 == d : {0}", 69.6875 == d);
+
+            float e = 0.1f;
+            Console.WriteLine("x : {0}", x);
+
+            double f = (double)e;
+            Console.WriteLine("y : {0}", f);
+            Console.WriteLine("0.1 == f : {0}", 0.1 == f);
+
+            // 부호있는 정수형식과 부호없는 정수형식 사이의변환
+            Console.WriteLine("=============================================");
+            Console.WriteLine("부호있는 정수형식과 부호없는 정수형식 사이의 변환");
+
+            int g = 500;
+            uint h = (uint)g;
+            Console.WriteLine("g(int) : {0}, h(uint) : {1}", g, h);
+
+            int i = -30;
+            uint j = (uint)i; // u : unsigned (부호 없는)
+            Console.WriteLine("i(int) : {0}, j(uint) : {1}", i, j); // 언더플로우 발생.
+
+            // 부동 소수점 형식과 정수 형식 사이의 변환
+            Console.WriteLine("=============================================");
+            Console.WriteLine("부동 소수점 형식과 정수 형식 사이의 변환");
+
+            float k = 0.9f;
+            int l = (int)a;
+            Console.WriteLine("k(float) : {0}, l(int) : {1}", k, l);
+
+            float m = 1.1f;
+            int n = (int)m;
+            Console.WriteLine("m(float) : {0}, n(int) : {1}", m, n);
+
+            // 문자열을 숫자로, 숫자를 문자열로 변환
+            Console.WriteLine("=============================================");
+            Console.WriteLine("문자열을 숫자로, 숫자를 문자열로 변환");
+
+            // int p = (int)o; => (int) 형식변환 연산자는 string 에 대해 언박싱이 불가. (string) 역시 마찬가지.
+            // 정수 계열 및 부동 소수점 형식의 Parse() 메소드를 이용하면, int/float 으로 형변환 가능.
+            int o = int.Parse("12345");
+            float p = float.Parse("123.45");
+            Console.WriteLine("m(float) : {0}, n(int) : {1}", m, n);
+
+            // 정수 계열 및 부동 소수점 형식의 ToString() 메소드를 이용하면, string 으로 형변환 가능.
+            int q = 12345;
+            string r = q.ToString();
+            Console.WriteLine("q(int) : {0}, r(string) : {1}", q, r);
+
+            float s = 123.45f;
+            string t = s.ToString();
+            Console.WriteLine("s(float) : {0}, t(string) : {1}", s, t);
+        }
+
+        /// <summary>
+        /// 예제 10. 상수(Constants)에 대해 알아본다.
+        /// </summary>
+        public void sample10()
+        {
+            const int MAX_INT = int.MaxValue;
+            const int MIN_INT = int.MinValue;
+
+            Console.WriteLine("MAX_INT : {0}, MIN_INT : {1}", MAX_INT, MIN_INT);
+        }
+
+        /// <summary>
+        /// 예제 11. 열거형식(Enumerator)에 대해 알아본다.
+        /// </summary>
+        enum DialogResult { YES, NO, CANCEL, CONFIRM, OK };
+        public void sample11()
+        {
+            Console.WriteLine((int)DialogResult.YES);
+            Console.WriteLine((int)DialogResult.NO);
+            Console.WriteLine((int)DialogResult.CANCEL );
+            Console.WriteLine((int)DialogResult.CONFIRM );
+            Console.WriteLine((int)DialogResult.OK );
+        }
+
+        /// <summary>
+        /// 예제 12. Nullable 형식에 대해 알아본다.
+        /// </summary>
+        public void sample12()
+        {
+            int? a = null;
+            Console.WriteLine(a.HasValue);
+            Console.WriteLine(a != null);
+
+            a = 3;
+            Console.WriteLine(a.HasValue);
+            Console.WriteLine(a != null);
+            Console.WriteLine(a.Value);
+
+            try
+            {
+                a = null;
+                Console.WriteLine(a.Value); // InvalidOperationException 예외 발생.
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                //Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 예제 13. 암시적 형식, var 에 대해 알아본다.
+        /// </summary>
+        public void sample13()
+        {
+            var a = 20; // var 키워드로 변수 선언시에는 반드시 초기화 필요.
+            var b = 3.1414213;
+            var c = "Hello, World!";
+            var d = new int[] { 10, 20, 30 };
+
+            Console.WriteLine("a => Type : {0}, Value : {1}", a.GetType(), a);
+            Console.WriteLine("b => Type : {0}, Value : {1}", b.GetType(), b);
+            Console.WriteLine("c => Type : {0}, Value : {1}", c.GetType(), c);
+            Console.Write("d => Type : {0}, Value : {1}", d.GetType(), d);
+            foreach (var item in d)
+                Console.Write("{0} ", item);
+
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// 예제 14. 공용형식시스템(CTS, Common Type System)에 대해 알아본다.
+        /// </summary>
+        public void sample14()
+        {
+            System.Int32 a = 123;
+            int b = 456;
+
+            Console.WriteLine("a type:{0}, value:{1}", a.GetType().ToString(), a);
+            Console.WriteLine("b type:{0}, value:{1}", b.GetType().ToString(), b);
+
+            System.String c = "abc";
+            string d = "def";
+
+            Console.WriteLine("c type:{0}, value:{1}", c.GetType().ToString(), c);
+            Console.WriteLine("d type:{0}, value:{1}", d.GetType().ToString(), d);
+        }
+
+        /// <summary>
+        /// 연습문제
+        /// </summary>
+        public void sample15()
+        {
+            // 1번
+            Console.WriteLine("{0}, {1}, {2}, {3} 이 잘못되었다.", "int a = 7.3;", "float b = 3.14;", "char d \"abc\";", "string e = \'한\';");
+            Console.WriteLine("int a = 7.3; => {0}", "double 에서 int 로 변경 불가. 해결책 : (int)");
+            Console.WriteLine("float b = 3.14; => {0}", "double 고정값(literal) 을 암시적(implicitly) 형변환 불가. 해결책 : 3.14f");
+            Console.WriteLine("char d \"abc\"; => {0}", "string 에서 char 로 암시적 형변환 불가. 해결책 : 1. a만 남겨놓고, \" => \'. 2. char => string 으로 변경");
+            Console.WriteLine("string e = \'한\'; => {0}", "char 에서 string 로 암시적 형변환 불가. 해결책 : 1. string => char 2. \' => \"\"");
+
+            // 2번
+            Console.WriteLine("값 형식은 값을 담는 데이터 형식을 말한다.(스택에 값이 저장된다.)");
+            Console.WriteLine("참조 형식은 값이 있는 곳의 위치(메모리주소)를 담는 데이터 형식이다.(힙에 값이 저장된다.)");
+            Console.WriteLine();
+
+            // 3번
+            Console.WriteLine("박싱은, object 형식에 값형식의 데이터를 대입하는 경우 일어난다. (힙에 값이 저장된다.)");
+            Console.WriteLine("언박싱은, 힙에 있는 값형식의 데이터를 값형식 객체에 다시 할당(형변환)하는 경우 일어난다. (스택에 값이 복사된다.)");
+            Console.WriteLine();
+
+            // 4번
+            Console.WriteLine("사각형의 너비를 입력하세요.");
+            string width = Console.ReadLine();
+
+            Console.WriteLine("사각형의 높이를 입력하세요.");
+            string height = Console.ReadLine();
+
+            int result = int.Parse(width) * int.Parse(height);
+            Console.WriteLine("사각형의 넓이는 {0} 입니다.", result);
+
+            // 5번
+            var a = 2020; // int
+            var b = "double"; // string
+            Console.WriteLine("a 는 int 형식이고, b는 string 형식이다.");
         }
     }
 }
