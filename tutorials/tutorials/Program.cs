41298e1e247128ff74bc9aa80ef7f33dc4280886
Parkseokje <sjpark@clipplr.com>
2016-04-21 10:57:20 +0900

    chapter04

--- tutorials/tutorials/Program.cs
+++ tutorials/tutorials/Program.cs
@@ -10,7 +10,25 @@ namespace tutorials
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Dreamonesys!");
+            // Formatting
+            // 정수를 10진수로 출력하기
+            int a = 3;
+            Console.WriteLine ("{0:D5}", a); // 출력 결과 00003
+            // 정수를 16진수로 출력하기
+            int b = 255;
+            Console.WriteLine("{0:X8}", b); // 000000FF
+
+            // 비트 논리 연산 - 논리곱(AND) 연산자
+            int result = 9 & 10;
+            Console.WriteLine(result);
+
+            // 비트 논리 연산 - 보수(NOT) 연산자
+            int c = 255;
+
+            //0x 는 16진수를 나타내는 prefix 이다.
+            //X는 16진수 8은 자릿수
+            Console.WriteLine("~{0}(0x{0:X8}) : {1}(0x{1:X8})", c, -c);
         }
     }
+
 }
