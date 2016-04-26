c86620f850a801b3cd1efa540f70a8a2e4135b76
Parkseokje <sjpark@clipplr.com>
2016-04-26 17:17:30 +0900

    initial commit.

--- tutorials/tutorials/Program.cs
+++ tutorials/tutorials/Program.cs
@@ -10,7 +10,31 @@ namespace tutorials
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Dreamonesys!");
+            Console.Write("반복 횟수를 입력하세요 : ");
+            var readText = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(readText))
+            {
+                Console.WriteLine("빈 값을 입력할 순 없습니다.");
+                return;
+            }
+
+            var count = Int32.Parse(readText);
+            if (count < 0)
+            {
+                Console.WriteLine("0 보다 작거나 같은 수를 입력할 수 없습니다.");
+                return;
+            }
+            else
+            {
+                int a = 1;
+                while (a <= count)
+                {
+                    Console.Write(new string('*', a));
+                    Console.WriteLine();
+                    a++;
+                }
+            }
         }
     }
 }
